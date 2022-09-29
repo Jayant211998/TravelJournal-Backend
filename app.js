d8fb2ff4b8d2f304784c9995ceff6ca4ff9c2693
Jayant211998 <jayantgawali98@gmail.com>
2022-09-29 16:26:05 +0530

    reset password

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ dotenv.config();
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', `${process.env.FRONT_END}`);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
